98bbd0a7346c06a1cbced1258f321fb002fd003d
LunaLynx <lynxluna12@gmail.com>
2023-02-03 13:24:58 +0200

    BoostEngineHelper v1.1

--- BoostEngineHelper/BoostEngineHelper.py
+++ BoostEngineHelper/BoostEngineHelper.py
@@ -1,4 +1,24 @@
 import psutil
+import logging
+import os
+
+# Set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Define a file handler
+if os.path.exists("BoostEngineHelper.log"):
+    os.remove("BoostEngineHelper.log")
+    
+file_handler = logging.FileHandler("BoostEngineHelper.log")
+file_handler.setLevel(logging.INFO)
+
+# Define a formatter
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+file_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(file_handler)
 
 # Get set of all active processes
 processes = set()
@@ -7,7 +27,7 @@ for process in psutil.process_iter():
         if not process.name().startswith("svchost"):
             processes.add(process.name())
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
+        logger.error(f"Failed to retrieve information about process {process}")
 
 # Get list of all active services
 services = []
@@ -15,21 +35,18 @@ for service in psutil.win_service_iter():
     if service.status() == "running":
         services.append(service.name())
 
-# Write output to a text file
-with open("BoostEngineHelper.txt", "w") as file:
-    file.write("BoostEngineHelper Version 1.0\n")
-    file.write("\n=====================\n")
-    file.write("Active processes:\n")
-    for process in processes:
-        file.write(process + "\n")
+# Write output to a log file
+logger.info("BoostEngineHelper Version 1.1")
+logger.info("=====================")
+logger.info("Active processes:")
+for process in processes:
+    logger.info(process)
 
-    file.write("\n=====================\n")
-    file.write("\nActive services:\n")
-    for service in services:
-        file.write(service + "\n")
+logger.info("=====================")
+logger.info("Active services:")
+for service in services:
+    logger.info(service)
 
-    file.write("\n=====================\n")
-    file.write("While closing processes and srvices, please be careful not to close any important processes or services...")
-
+logger.warning("While closing processes and services, please be careful not to close any important processes or services...")
 # Print message to confirm that output has been written to a file
-print("Processes, services, and background apps have been written to a file.")
+print("The output has been successfully written to a .log file, located in the same directory.")
